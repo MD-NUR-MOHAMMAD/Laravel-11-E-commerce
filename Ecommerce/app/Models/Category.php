3a4478b61d8980023b3262863a549810fbe300a8
MD-NUR-MOHAMMAD <152972252+MD-NUR-MOHAMMAD@users.noreply.github.com>
2024-11-09 18:44:45 +0600

    Subcategory and Category model modify

--- Ecommerce/app/Models/Category.php
+++ Ecommerce/app/Models/Category.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [];
+    public function subcategories(): HasMany
+    {
+        return $this->hasMany(subcategory::class);
+    }
+    public function products(): HasMany
+    {
+        return $this->hasMany(product::class);
+    }
+    public function Images()
+    {
+        return $this->hasManyThrough(image::class, product::class);
+    }
 }
--- Ecommerce/app/Models/Subcategory.php
+++ Ecommerce/app/Models/Subcategory.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Subcategory extends Model
 {
-    //
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [];
+    /**
+     * Get the post that owns the comment.
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(category::class);
+    }
+    public function products(): HasMany
+    {
+        return $this->hasMany(product::class);
+    }
 }
