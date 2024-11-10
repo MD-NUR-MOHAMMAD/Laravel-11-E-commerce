697310f07b60fb9cfd883a7819db59078814714c
MD-NUR-MOHAMMAD <152972252+MD-NUR-MOHAMMAD@users.noreply.github.com>
2024-11-10 23:52:42 +0600

    Middleware added

--- Ecommerce/routes/web.php
+++ Ecommerce/routes/web.php
@@ -16,6 +16,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('admin')->group(function () {
+
+
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
